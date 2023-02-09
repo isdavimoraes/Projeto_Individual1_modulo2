923f473d39428d9a161a7803015deb725c730798
isdavimoraes <isdavimoraes@gmail.com>
2023-02-09 00:43:14 -0300

    Update proje_ind1_mod2.py

--- proje_ind1_mod2.py
+++ proje_ind1_mod2.py
@@ -1,22 +1,16 @@
-'''
-Criar com Python uma lista para armazenar esses resultados
-(e outros resultados que quiser no mesmo formato, o código
-precisa funcionar para qualquer lista que seja inserida nesse
-formato) e depois uma função que busca o candidato de
-acordo com os critérios digitados pelo usuário.
-candidato = input('Nome do candidato: ')
-
-'''
-
+######################################
+#  Davi de Moraes Novaes             #
+#  Modulo 2 - Projeto Individual 1   #
+######################################
 def avaliação():
-    sair = 's'
-    while sair != 'n':
-        lista_n.append(input('Nome do candidato: '))
+    sair = 's'  #menu de sair
+    while sair != 'n':  #puxando o while para atribuir mais de 1 elementa para a lista
+        lista_n.append(input('Nome do candidato: ')) #adicionando nome da lista 
         e = float(input('Nota do candidato referente a ENTREVISTA: '))
         t = float(input('Nota do candidato referente a TESTE TEÓRICO: '))
         p = float(input('Nota do candidato referente a TESTE PRÁTICO: '))
         s = float(input('Nota do candidato referente a AVALIÇÃO SOFT SKILLS: '))
-        lista_not.append(f'e{e}_t{t}_p{p}_s{s}')
+        lista_not.append(f'e{e}_t{t}_p{p}_s{s}') #adicionando os valores de e,t,p e s na lista
         print(f'Candidato {lista_n} adicionado na lsita com nota: {lista_not}')        
         print('Continuar? [S]im ou [N]ão? ')
         sair = input().lower()
@@ -26,7 +20,7 @@ lista_n = []
 avaliação()
 print('*'*45)
 
-def pesquisa():
+def pesquisa(): #função def para realizar as pesquisas
     sair = ''
     while sair != 'n':          
         e = float(input('Pesquisa de nota da ENTREVISTA: '))
@@ -34,7 +28,7 @@ def pesquisa():
         p = float(input('Pesquisa de nota do TESTE PRÁTICO: '))
         s = float(input('Pesquisa de nota da AVALIÇÃO SOFT SKILLS: '))
         procura = f'e{e}_t{t}_p{p}_s{s}'
-        for i,cand in enumerate(lista_not):
+        for i,cand in enumerate(lista_not): #Usando for com enumerate para realizar a busca
             if cand == procura:
                 print(f'O canditato aprovado seguindo os parâmetros estabelecidos foi : {lista_n[i]}')      
         print('Continuar? [S]im ou [N]ão? ')
